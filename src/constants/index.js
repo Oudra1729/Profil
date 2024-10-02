31f91250a23ef5cea0ed32dc455631b02d6fb739
oudra brahim <brahimbra12356@gmail.com>
2024-10-02 19:41:39 +0100

    "Added framer-motion dependency, updated package-lock.json, and modified App.jsx, constants/index.js files"

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Marrakesh-safi",
+  phoneNo: "+212 9596 80 53",
+  email: "oudra.brahim.99@example.com",
 };
